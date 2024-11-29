e0a0f0b9ef87582f0fb538fa07f92780c867eb2c
peculiar <peculiarbabalola@gmail.com>
2024-11-29 23:43:17 +0100

    chore: changed to http

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
 import App from './App';
-import { DatabaseService } from './Infrastructure/Database/DatabaseService';
 import { APP_NAME } from './Core/Types/Constants';
 
 dotenv.config();
 
 const startApp = async () => {
     try {
-        const port = process.env.PORT || 3000;
+        const port = process.env.PORT || 3002;
         
         const server = App.listen(port, () => {
-            console.log(`${APP_NAME} Server is running on port https://localhost:${port}`);
+            console.log(`${APP_NAME} Server is running on port http://localhost:${port}`);
         });
 
         // Handle graceful shutdown
         process.on('SIGTERM', async () => {
             console.log('SIGTERM signal received');
-            await DatabaseService.shutdown();
             server.close(() => {
                 console.log('Server closed');
                 process.exit(0);
