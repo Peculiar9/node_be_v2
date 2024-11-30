e2a3d743798d7e6a6fb8ae3721b916bee4aeebf1
peculiar <peculiarbabalola@gmail.com>
2024-11-30 21:56:59 +0100

    chore: routing and middleware hierachy

--- src/App.ts
+++ src/App.ts
@@ -1,22 +1,25 @@
+import 'reflect-metadata';
 import cors from 'cors'
 import bodyParser from 'body-parser';
-import 'reflect-metadata';
+import { Container } from 'inversify';
 import { InversifyExpressServer } from 'inversify-express-utils';
-import express, { Response, Request, NextFunction } from 'express';
-import path from 'path';
 import { DatabaseService } from './Infrastructure/Database/DatabaseService';
+import { getRouteInfo } from 'inversify-express-utils';
+
+import './Controllers/InitController';
+import './Controllers/auth/AccountController';
+
 import { DIContainer } from './Core/DIContainer';
 
-import './Controllers/ControllerShave';
-import './Controllers/auth/AccountController';
-import { Container } from 'inversify';
-
+import express, { Response, Request, NextFunction } from 'express';
+import path from 'path';
 class App {
     public app: any;
 
     constructor() {
         this.app = express();
         this.initialize();
+        console.log("App initialized")
     }
 
     private async initialize() {
@@ -38,12 +41,17 @@ class App {
                 app.set('view engine', 'ejs');
                 app.set('views', path.join(__dirname, '..', 'src', 'static'));
                 app.use(cors());
+                console.log("Setting config.....");
             });
+            
 
+            console.log("Building server.....");
             this.app = server.build();
             this.initErrorHandling();
             // Graceful shutdown
             this.setupGracefulShutdown(container);
+            const routeInfo = getRouteInfo(container);
+            console.log(JSON.stringify(routeInfo, null, 2));
         } catch (error: any) {
             console.error("App initialization error:", error.message);
             process.exit(1);
