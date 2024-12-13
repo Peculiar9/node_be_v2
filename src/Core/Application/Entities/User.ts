507a8cc6d98eb2b375baea9ca1d9f20447bd6bd5
peculiar <peculiarbabalola@gmail.com>
2024-12-13 10:10:40 +0100

    chore: Column decorator for entity meta data to set entity data on creation and initialization

--- src/Core/Application/Entities/User.ts
+++ src/Core/Application/Entities/User.ts
@@ -1,57 +1,99 @@
 import { IUser } from '../Interface/Entities/auth-and-user/IUser';
 import { CreateUserDTO, UpdateUserDTO } from '../DTOs/UserDTO';
 import { ValidationError } from '../Error/AppError';
-import { UtilityService } from '@/Core/Services/UtilityService';
+import { UtilityService } from '../../Services/UtilityService';
 import { UserRole } from '../Enums/UserRole';
+import { Column, CompositeIndex, Index } from '../../../extensions/decorators';
 
+@CompositeIndex(['first_name', 'last_name'])
 export class User implements IUser {
+    @Column('UUID PRIMARY KEY DEFAULT gen_random_uuid()')
     public _id?: string | null | undefined;
-    public first_name: string | null | undefined;
-    public last_name: string | null | undefined;
-    public email: string | null | undefined;
-    public password: string | null | undefined;
+
+    @Column('VARCHAR(255) NOT NULL')
+    public first_name: string;
+
+    @Column('VARCHAR(255) NOT NULL')
+    public last_name: string;
+
+    @Index({ unique: true })
+    @Column('VARCHAR(255) UNIQUE NOT NULL')
+    public email: string;
+
+    @Column('VARCHAR(255) NOT NULL')
+    public password: string;
+
+    @Column('VARCHAR(255) DEFAULT NULL')
     public profile_image: string | null | undefined;
-    public status: string | null | undefined;
-    public isActive: boolean;
+
+    @Column('VARCHAR(50) NOT NULL DEFAULT \'inactive\'')
+    public status: string;
+
+    @Column('BOOLEAN DEFAULT false')
+    public is_active: boolean;
+
+    @Column('TEXT DEFAULT NULL')
     public user_secret: string | null | undefined;
+
+    @Column('VARCHAR(255) DEFAULT NULL')
     public salt: string | null | undefined;
+
+    @Column('TEXT DEFAULT NULL')
     public refresh_token: string | null | undefined;
+
+    @Column('TIMESTAMP DEFAULT CURRENT_TIMESTAMP')
     public last_login: string | null | undefined;
-    public roles: string[];
-    public created_at: string | null | undefined;
-    public updated_at: string | null | undefined;
+
+    @Column('TEXT[] DEFAULT ARRAY[]::TEXT[]')
+    public roles: string[] | UserRole[] = [];
+
+    @Index()
+    @Column('TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP')
+    public created_at: string;
+
+    @Index()
+    @Column('TIMESTAMP DEFAULT CURRENT_TIMESTAMP')
+    public updated_at: string;
+
+    @Column('INTEGER DEFAULT 0')
     public __v: number;
 
     private constructor(data: Partial<IUser>) {
         Object.assign(this, data);
     }
 
-    static async createFromDTO(dto: CreateUserDTO): Promise<User> {
-        const { hash: password, salt } = await UtilityService.generatePasswordHash(dto.password);
-        const user_secret = UtilityService.generateUserSecret();
-        
-        const userData: Partial<IUser> = {
-            _id: UtilityService.generateUUID(),
-            first_name: dto.first_name,
-            last_name: dto.last_name,
-            email: dto.email.toLowerCase(),
-            password,
-            salt,
-            user_secret,
-            profile_image: dto.profile_image || '',
-            status: 'active',
-            isActive: true,
-            refresh_token: '',  // Initialize with empty string
-            last_login: '',     // Initialize with empty string
-            roles: dto.roles,
-            created_at: new Date().toISOString(),
-            updated_at: new Date().toISOString(),
-            __v: 0
-        };
+    static async createFromDTO(dto: CreateUserDTO): Promise<User | undefined> {
+        try{
+            const { hash: password, salt } = await UtilityService.generatePasswordHash(dto.password);
+            const user_secret = UtilityService.generateUserSecret();
+            const userData: Partial<IUser> = {
+                // _id: UtilityService.generateUUID(),
+                first_name: dto.first_name,
+                last_name: dto.last_name,
+                email: dto.email.toLowerCase(),
+                password,
+                salt,
+                user_secret,
+                profile_image: dto.profile_image || '',
+                status: 'active',
+                is_active: true,
+                refresh_token: '',  // Initialize with empty string
+                roles: dto.roles,
+                created_at: new Date().toISOString(),
+                updated_at: new Date().toISOString(),
+                __v: 0
+            };
 
-        const user = new User(userData);
-        user.validate();
-        return user;
+            const user = new User(userData);
+            user.validate();
+            return user;
+        }catch(error: any){
+            console.error('User Object creation: ', {
+                message: error.message,
+                stack: error.stack
+            });
+            throw error;
+        }
     }
 
     static async updateFromDTO(existingUser: User, dto: UpdateUserDTO): Promise<User> {
