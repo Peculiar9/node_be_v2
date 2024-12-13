29fd8c8852aea9c15a8e2b6d239b42c59a698131
peculiar <peculiarbabalola@gmail.com>
2024-12-13 10:11:31 +0100

    chore: create user method

--- src/Core/Application/Interface/Services/IAuthService.ts
+++ src/Core/Application/Interface/Services/IAuthService.ts
@@ -1,4 +1,5 @@
 import { LoginResponseDTO } from '../../DTOs/AuthDTO';
+import { CreateUserDTO } from '../../DTOs/UserDTO';
 
 export interface IAuthService {
     authenticate(email: string, password: string): Promise<LoginResponseDTO>;
@@ -6,4 +7,5 @@ export interface IAuthService {
     refreshAccessToken(refreshToken: string): Promise<{ accessToken: string }>;
     revokeRefreshToken(userId: string): Promise<void>;
     getUserFromToken(userId: string): Promise<any>;
+    createUser(createUserDto: CreateUserDTO): Promise<any>;
 }
